d25c6d5379d75c9146f88d39c028f15fd9398b13
ambosing <ambosing_@naver.com>
2023-01-08 22:38:49 +0900

    Update knight_weapon.py

--- jiwon/knight_weapon.py
+++ jiwon/knight_weapon.py
@@ -1,26 +1,15 @@
-
-def get_divisor(num):
-    n = 2
-    pf = 1
-    c = 0
-    while num > 1:
-        if num % n == 0:
-            num //= n
-            c += 1
-        else:
-            pf *= (c + 1)
-            n += 1
-            c = 0
-    pf *= (c + 1)
-    return pf
+from math import sqrt
 
 
 def solution(number, limit, power):
-    answer = 1
-    for i in range(2, number + 1):
-        divisor = get_divisor(i)
-        if divisor > limit:
-            answer += power
-        else:
-            answer += divisor
+    answer = 0
+    for num in range(1, number + 1):
+        sqrt_num = int(sqrt(num))
+        count = 0
+        for n in range(1, sqrt_num + 1):
+            if num == n * n:
+                count += 1
+            elif num % n == 0:
+                count += 2
+        answer += count if count <= limit else power
     return answer
