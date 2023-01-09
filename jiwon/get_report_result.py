cf01fe06aa423ddfe9589d999c52cf892c1e9347
ambosing <ambosing_@naver.com>
2023-01-09 23:01:51 +0900

    Update get_report_result.py

--- jiwon/get_report_result.py
+++ jiwon/get_report_result.py
@@ -1,20 +1,16 @@
 def solution(id_list, report, k):
-    answer = [0 for i in id_list]
-    reporters = {key: i for i, key in enumerate(id_list)}
-    reported_cnt = dict()
+    answer = [0 for _ in id_list]
     reported_list = dict()
     for r in set(report):
         reporter, reported = r.split(" ")
         if reported in reported_list:
-            reported_cnt[reported] += 1
             reported_list[reported].add(reporter)
         else:
             reported_list[reported] = {reporter}
-            reported_cnt[reported] = 1
 
     for key, value in reported_list.items():
-        if reported_cnt[key] < k:
+        if len(reported_list[key]) < k:
             continue
         for v in value:
-            answer[reporters[v]] += 1
+            answer[id_list.index(v)] += 1
     return answer
