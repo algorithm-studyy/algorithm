0624020d9b381f0789404c37673444d2bbd5df08
CheeseB <2489ckckck@naver.com>
2023-01-15 21:30:42 +0900

    백준 1874번(스택 수열)

--- SeongYeon/baekjoon/10828.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const inputFile = 'input.txt';
-const inputList = fs.readFileSync(inputFile).toString().split('\n');
-
-const stack = [];
-let top = -1;
-inputList.forEach((input) => {
-  console.log(input);
-});
