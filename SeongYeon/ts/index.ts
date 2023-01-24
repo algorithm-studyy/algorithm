57cc5f468249810c56e4ea7f0b454cf63de535ce
CheeseB <2489ckckck@naver.com>
2023-01-24 21:26:01 +0900

    폴더정리, TS 세팅

---
+++ SeongYeon/ts/index.ts
@@ -0,0 +1,7 @@
+const solution = (input: string) => {
+  let result = input;
+  console.log(result);
+};
+
+const input = require('fs').readFileSync('test/input.txt').toString().trim();
+solution(input);
