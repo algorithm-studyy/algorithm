90dd8f94c99480410d9dca97b905a3b011e4c017
CheeseB <2489ckckck@naver.com>
2023-02-14 22:43:35 +0900

    1로 만들기

--- SeongYeon/ts/1463.ts
+++ SeongYeon/ts/1463.ts
@@ -6,13 +6,13 @@ const input: string = require('fs')
 
 // variable
 const n: number = Number(input);
-let dp: number[] = [0, 0, 1, 1];
+let dp: number[] = [0, 0];
 
 // solution
-for (let i = 4; i <= n; i++) {
-  if (i % 3 === 0) dp[i] = Math.min(dp[i - 1]!, dp[i / 3]!) + 1;
-  else if (i % 2 === 0) dp[i] = Math.min(dp[i - 1]!, dp[i / 2]!) + 1;
-  else dp[i] = dp[i - 1]! + 1;
+for (let i = 2; i <= n; i++) {
+  dp[i] = dp[i - 1]! + 1;
+  if (i % 2 === 0) dp[i] = Math.min(dp[i]!, dp[i / 2]! + 1);
+  if (i % 3 === 0) dp[i] = Math.min(dp[i]!, dp[i / 3]! + 1);
 }
 
 // output
