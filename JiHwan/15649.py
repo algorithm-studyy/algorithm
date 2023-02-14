9e6b76acb5a0969b4298a575487872d8be660fda
zeno1030 <zeno1030@naver.com>
2023-02-14 11:28:47 +0900

    test

--- JiHwan/15649.py
+++ JiHwan/15649.py
@@ -1,23 +1,16 @@
 n,m = map(int,input().split())
 
 stack = []
+
+
 def dfs():
     for i in range(1, n+1):
         if i not in stack:
             stack.append(i)
             dfs()
             stack.pop()
-            
-            
-            
-
-    if len(stack)==n:
+    if len(stack)==m:
         print(' '.join(map(str,stack)))
-            
-            
-            
-            
+
+
 dfs()
-
-            
-
