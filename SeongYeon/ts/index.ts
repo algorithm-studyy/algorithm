0d7208624143dafed4a44517bfcaa1060f6856f9
CheeseB <2489ckckck@naver.com>
2023-02-22 22:35:06 +0900

    이친수 리팩토링

--- SeongYeon/ts/index.ts
+++ SeongYeon/ts/index.ts
@@ -6,21 +6,12 @@ const input: string = require('fs')
 
 // variable
 const n: number = Number(input);
-const dp: bigint[][] = [
-  [BigInt(0), BigInt(0)],
-  [BigInt(0), BigInt(1)],
-];
+const dp: bigint[] = [0, 1, 1].map((num: number) => BigInt(num));
 
 // solution
-for (let i = 2; i <= n; i++) {
-  dp[i] = [BigInt(0), BigInt(0)];
-  if (dp[i - 1]![0]) {
-    dp[i]![0] += dp[i - 1]![0]!;
-    dp[i]![1] += dp[i - 1]![0]!;
-  }
-  if (dp[i - 1]![1]) dp[i]![0] += dp[i - 1]![1]!;
+for (let i = 3; i <= n; i++) {
+  dp[i] = dp[i - 1]! + dp[i - 2]!;
 }
 
 // output
-const result = dp[n]![0]! + dp[n]![1]!;
-console.log(result.toString());
+console.log(dp[n]!.toString());
