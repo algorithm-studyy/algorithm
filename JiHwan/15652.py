3228fd1ca5a1cf84b763bb2458117c1c9f0a57b5
zeno1030 <zeno1030@naver.com>
2023-02-28 14:32:17 +0900

    n과m4번완료

--- JiHwan/15652.py
+++ JiHwan/15652.py
@@ -4,17 +4,14 @@ stack = []
 
 
 def dfs():
-    for i in range(1, n + 1):
-        stack.append(i)
-        dfs()
-        stack.pop()
-
     if len(stack) == m:
         print(' '.join(map(str, stack)))
         return
 
+    for i in range(1, n+1):
+        stack.append(i)
+        dfs()
+        stack.pop()
+
 
 dfs()
-
-
-
