4648a19942fedb9c87b6766d0f01cba49fec729b
ambosing <ambosing_@naver.com>
2023-03-01 09:57:46 +0900

    Update leave_dp_14501.py

--- jiwon/boj/leave_dp_14501.py
+++ jiwon/boj/leave_dp_14501.py
@@ -2,9 +2,8 @@ def solution():
     n = int(input())
     arr = [list(map(int, input().split(' '))) for _ in range(n)]
     dp = [0] * 76
-    max_val = 0
     for i in range(n):
-        max_val = max(max_val, dp[i - 1])
+        dp[i] = dp[i - 1] if dp[i - 1] != 0 and dp[i - 1] > dp[i] else dp[i]
         if i + arr[i][0] <= n:
             dp[i + arr[i][0]] = max(arr[i][1] + dp[i], dp[i + arr[i][0]])
     print(max(dp))
