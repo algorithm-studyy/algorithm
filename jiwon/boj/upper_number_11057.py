ea755383a80d736a64cc9290cb639dbe7d5acb2d
ambosing <ambosing_@naver.com>
2023-03-06 21:53:29 +0900

    백준 오르막 수

---
+++ jiwon/boj/upper_number_11057.py
@@ -0,0 +1,10 @@
+def solution():
+    n = int(input())
+    dp = [1] * 10
+    for i in range(1, n):
+        for j in range(1, 10):
+            dp[j] = (dp[j] + dp[j - 1]) % 10_007
+    print(sum(dp) % 10_007)
+
+
+solution()
