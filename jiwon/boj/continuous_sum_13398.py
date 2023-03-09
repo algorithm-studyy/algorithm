7ea03fbd047c9ee9a410a7dcf9243c25cf45b2e8
Yang JiWon <ambosing_@naver.com>
2023-03-09 16:21:53 +0900

    Update continuous_sum_13398.py

--- jiwon/boj/continuous_sum_13398.py
+++ jiwon/boj/continuous_sum_13398.py
@@ -4,24 +4,12 @@ from sys import maxsize
 def solution():
     n = int(input())
     arr = list(map(int, input().split(' ')))
-    dp = [-maxsize] * n
-    sub = None
-    dp[0] = arr[0]
+    dp = [[-maxsize] * n for _ in range(2)]
+    dp[0][0], dp[1][0] = arr[0], arr[0]
     for i in range(1, n):
-        if arr[i] < 0:
-            if not sub:
-                dp[i] = dp[i - 1]
-                sub = arr[i]
-                continue
-            if sub > arr[i]:
-                dp[i] = dp[i - 1] + sub
-                sub = arr[i]
-            else:
-                dp[i] = dp[i - 1] + arr[i]
-        else:
-            dp[i] = dp[i - 1] + arr[i] if dp[i - 1] > 0 else arr[i]
-    print(max(dp))
-    print(dp)
+        dp[0][i] = max(dp[0][i - 1] + arr[i], arr[i])
+        dp[1][i] = max(dp[1][i - 1] + arr[i], dp[0][i - 1])
+    print(max(max(dp[1]), max(dp[0])))
 
 
 solution()
