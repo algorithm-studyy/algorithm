9feb6e2d35b54e922d9efb0686fc519b4e2a383c
CheeseB <2489ckckck@naver.com>
2023-03-14 22:09:43 +0900

    단지번호붙이기 못품

---
+++ SeongYeon/ts/2667(notyet).ts
@@ -0,0 +1,13 @@
+// input
+const input: string[] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+// variable
+
+// solution
+
+// output
+console.log();
