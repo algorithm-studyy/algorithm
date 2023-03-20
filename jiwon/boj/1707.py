35d43b7a570f17d8929abfec5a6c6acad680b5f4
Yang JiWon <ambosing_@naver.com>
2023-03-20 15:12:37 +0900

    이분그래프는 다시풀기

--- jiwon/boj/1707.py
+++ jiwon/boj/1707.py
@@ -4,14 +4,18 @@ from collections import deque
 
 def bfs(s):
     q = deque([s])
-    visited[s] = True
+    visited[s] = 1
     while q:
         nv = q.popleft()
         for i in g[nv]:
-            if visited[i]:
+            if visited[i] != 0 and visited[nv] != visited[i]:
+                print(visited[nv], visited[i], nv, i)
+                return False
+            elif visited[i]:
                 continue
-            visited[i] = True
+            visited[i] = visited[nv] * -1
             q.append(i)
+    return True
 
 
 if __name__ == '__main__':
@@ -19,16 +23,24 @@ if __name__ == '__main__':
     for _ in range(n):
         v, e = map(int, stdin.readline().split(' '))
         g = [[] for _ in range(v)]
-        visited = [False] * v
+        visited = [0] * v
         for _ in range(e):
             a, b = map(int, stdin.readline().split(' '))
             g[a - 1].append(b - 1)
-            # g[b - 1].append(a - 1)
-        bfs(0)
-        print(visited)
-        if all(visited):
-            print('NO')
+            g[b - 1].append(a - 1)
+        for a in range(v):
+            if visited[a] == 0:
+                tf = bfs(a)
+                if not tf:
+                    print('NO')
+                    break
         else:
             print('YES')
+        print(visited)
 
-
+# 1
+# 5 4
+# 1 2
+# 3 4
+# 3 5
+# 4 5
