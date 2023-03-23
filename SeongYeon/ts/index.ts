dba922a472f3d6356b3fb020d5aaee851dfd80a7
CheeseB <2489ckckck@naver.com>
2023-03-23 22:31:31 +0900

    이모티콘 못품

--- SeongYeon/ts/index.ts
+++ SeongYeon/ts/index.ts
@@ -1,45 +1,10 @@
 // input
-const input: string[] = require('fs')
+const input: string = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
-  .toString()
-  .split('\n');
+  .toString();
 
 // variable
-const [n, m] = input[0]!.split(' ').map(Number);
-const edges: string[] = input.slice(1, 1 + m!);
-const graph: number[][] = Array.from(Array(n! + 1), () => new Array());
-const visited: boolean[] = Array(n! + 1).fill(false);
-let answer: number = 0;
-
-edges.forEach((row) => {
-  const [a, b] = row.split(' ').map(Number);
-  graph[a!]!.push(b!);
-  graph[b!]!.push(a!);
-});
 
 // solution
-const bfs = (startNode: number) => {
-  const queue = [startNode];
-
-  while (queue.length > 0) {
-    const nextNode: number = queue.shift()!;
-
-    graph[nextNode]!.forEach((node) => {
-      if (!visited[node]) {
-        visited[node] = true;
-        queue.push(node);
-      }
-    });
-  }
-};
-
-for (let i = 1; i <= n!; i++) {
-  if (!visited[i]) {
-    visited[i] = true;
-    bfs(i);
-    answer++;
-  }
-}
 
 // output
-console.log(answer);
