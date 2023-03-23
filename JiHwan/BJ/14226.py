4cfcf100cdb7b4e25d01e4143042358badb9a429
ZENO <37774859+zeno1030@users.noreply.github.com>
2023-03-23 22:43:08 +0900

    TOO HARD

---
+++ JiHwan/BJ/14226.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+
+def bfs(n):
+    queue = deque()
+    queue.append((1, 0))
+    graph[1][0] = 1
+    while queue:
+        s, c = queue.popleft()
+        if graph[s][s] == 0:
+            graph[s][s] = graph[s][c] + 1
+            queue.append((s, s))
+        if s+c <= n and graph[s+c][c] == 0:
+            graph[s+c][c] = graph[s][s] + 1
+            queue.append((s+c, c))
+        if
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    S = int(sys.stdin.readline())
+    graph = [[0] * (S+1) for _ in range(S+1)]
+    bfs(S)
