de59d092d78b52ccf9da132b0bb8647026fd88a9
CheeseB <2489ckckck@naver.com>
2023-04-13 22:12:42 +0900

    점프와 순간이동

---
+++ SeongYeon/js/programmers/jumpAndTeleport.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  let count = 1;
+
+  while (n > 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = n - 1;
+      count++;
+    }
+  }
+
+  return count;
+}
