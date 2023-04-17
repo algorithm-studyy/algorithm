dcbfef2970e56a29352c327ec41c693b0e50d191
zeno1030 <zeno1030@naver.com>
2023-04-17 16:29:20 +0900

    튜플 해결

--- JiHwan/BJ/tuple.py
+++ JiHwan/BJ/tuple.py
@@ -5,7 +5,6 @@ def solution(s):
     s.sort(key=len)
     for key in s:
         splited_key = key.split(',')
-        print(splited_key)
         for argument in splited_key:
             if int(argument) not in answer:
                 answer.append(int(argument))
