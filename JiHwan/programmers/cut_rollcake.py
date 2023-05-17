26290213857ce30b3580097d0079cd444dd7b3c2
ZENO <zeno1030@naver.com>
2023-05-17 22:30:29 +0900

    Counter란 존재를 잊지 말자

--- JiHwan/programmers/cut_rollcake.py
+++ JiHwan/programmers/cut_rollcake.py
@@ -1,10 +1,16 @@
+from collections import Counter
+
+
 def solution(topping):
     answer = 0
+    bro = Counter(topping)
+    a = set()
 
-    for i in range(1, len(topping)):
-        a = set(topping[i:])
-        b = set(topping[:i])
-        if len(a) == len(b):
+    for i in topping:
+        bro[i] -= 1
+        if bro[i] == 0:
+            bro.pop(i)
+        a.add(i)
+        if len(bro) == len(a):
             answer += 1
-
     return answer
