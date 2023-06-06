0086f30bc864399f38165c8cb60cac88e80fa4d5
ZENO <zeno1030@naver.com>
2023-06-06 21:28:37 +0900

    ã계속 시간초과발생 코드 수정후 제출

--- JiHwan/programmers/pickup.py
+++ JiHwan/programmers/pickup.py
@@ -1,14 +1,18 @@
 def solution(cap, n, deliveries, pickups):
-    answer = -1
-    sum_deliveries = sum(deliveries)
-    sum_pickups = sum(pickups)
-    while True:
-        if sum_deliveries == 0 and sum_pickups == 0:
-            return answer
-        else:
-            for i in deliveries[::-1]:
+    deliveries = deliveries[::-1]
+    pickups = pickups[::-1]
+    answer = 0
 
+    num_deli = 0
+    num_pick = 0
+    for i in range(n):
+        num_deli += deliveries[i]
+        num_pick += pickups[i]
 
+        while num_deli > 0 or num_pick > 0:
+            num_deli -= cap
+            num_pick -= cap
+            answer += (n - i) * 2
+        #            택배가 출발을 했으면 되돌아와야 하기 때문에 x2를 진행
 
-
-
+    return answer
