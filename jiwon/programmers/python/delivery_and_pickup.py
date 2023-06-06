f21ceec719e95285c54a36c773c94cb83699dbb4
ambosing <ambosing_@naver.com>
2023-06-06 23:45:46 +0900

    해결!

--- jiwon/programmers/python/delivery_and_pickup.py
+++ jiwon/programmers/python/delivery_and_pickup.py
@@ -1,16 +1,14 @@
 # 택배가 제일 멀리 있으면 일단 거기까지는 가야함 ..
-def get_not_zero_index(lst):
-    for idx in range(len(lst) - 1, -1, -1):
+def get_not_zero_index(lst, n):
+    for idx in range(n - 1, -1, -1):
         if lst[idx] != 0:
             return idx
-    return 0
+    return -1
 
 
 def delivery(d, i, cap):
     c = cap
-    if i <= 0:
-        return d, i
-    while c > 0:
+    while i >= 0 and c > 0:
         c -= d[i]
         d[i] = -c if c < 0 else 0
         while i >= 0 and d[i] == 0:
@@ -19,10 +17,8 @@ def delivery(d, i, cap):
 
 
 def pickup(p, j, cap):
-    c = cap
-    if j <= 0:
-        return p, j
-    while c < cap:
+    c = 0
+    while j >= 0 and c < cap:
         c += p[j]
         p[j] = c - cap if c > cap else 0
         while j >= 0 and p[j] == 0:
@@ -32,10 +28,10 @@ def pickup(p, j, cap):
 
 def solution(cap, n, d, p):
     answer = 0
-    i, j = get_not_zero_index(d), get_not_zero_index(p)
-    while i > 0 and j > 0:
-        c = cap
+    i, j = get_not_zero_index(d, n), get_not_zero_index(p, n)
+    while i >= 0 or j >= 0:
         answer += max(i + 1, j + 1)
         d, i = delivery(d, i, cap)
         p, j = pickup(p, j, cap)
-    return answer
+    return answer * 2
+
