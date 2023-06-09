b9203a07086c4b03cc70af4730bb2c0ab9e04910
Yang JiWon <ambosing_@naver.com>
2023-06-09 10:58:08 +0900

    프로그래머스 미로 탈출 성공

--- jiwon/programmers/python/escape_miro.py
+++ jiwon/programmers/python/escape_miro.py
@@ -14,21 +14,26 @@ def find_mark(m, mark):
                 return i, j
 
 
-def solution(m):
-    start, lever, ext = find_mark(m, 'S'), find_mark(m, 'L'), find_mark(m, 'E')
-    print(start, lever, ext)
+def bfs(m, s, e):
     r, c = len(m), len(m[0])
     v = [[-1] * c for _ in range(r)]
-    q = deque([start])
-    v[start[0]][start[1]] = 0
+    q = deque([s])
+    v[s[0]][s[1]] = 0
     while q:
         y, x = q.popleft()
+        if y == e[0] and x == e[1]:
+            return v[y][x]
         for i in range(4):
             ny, nx = y + dy[i], x + dx[i]
             if ny >= r or ny < 0 or nx >= c or nx < 0 or m[ny][nx] == 'X' or v[ny][nx] != -1:
                 continue
             v[ny][nx] = v[y][x] + 1
             q.append((ny, nx))
-    if v[lever[0]][lever[1]] == -1 or v[ext[0]][ext[1]] == -1:
-        return -1
-    return v[ext[0]][ext[1]]
+    return -1
+
+
+def solution(m):
+    start, lever, ext = find_mark(m, 'S'), find_mark(m, 'L'), find_mark(m, 'E')
+    lever_distance = bfs(m, start, lever)
+    ext_distance = bfs(m, lever, ext)
+    return -1 if lever_distance == -1 or ext_distance == -1 else lever_distance + ext_distance
