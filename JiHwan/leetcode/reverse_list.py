6c799b983ad9184fac210a0feabbb102cdb1d5c3
zeno1030 <zeno1030@naver.com>
2023-06-27 13:00:10 +0900

    ãleetcode ãreverse_list end

---
+++ JiHwan/leetcode/reverse_list.py
@@ -0,0 +1,8 @@
+#Time:210 ms Meomory20.7 MB
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        return s.reverse()
+
+        """
+        Do not return anything, modify s in-place instead.
+        """
