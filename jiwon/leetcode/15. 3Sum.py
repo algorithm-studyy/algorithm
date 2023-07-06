5fe94564705a3802db3ce7febd7cef644d2e51a8
ambosing <ambosing_@naver.com>
2023-07-06 22:20:26 +0900

    561번 풀이, 15번 수정

--- jiwon/leetcode/15. 3Sum.py
+++ jiwon/leetcode/15. 3Sum.py
@@ -1,11 +1,24 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        num_set = {nums[0]}
-        answer = set()
-        for i in range(1, len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if -(nums[i] + nums[j]) in num_set:
-                    answer.add(tuple(sorted([-(nums[i] + nums[j]), nums[i], nums[j]])))
-            num_set.add(nums[i])
+        nums.sort()
+        answer = []
+        for i, n in enumerate(nums):
+            if i > 0 and nums[i - 1] == n:
+                continue
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                val = n + nums[left] + nums[right]
+                if val < 0:
+                    left += 1
+                elif val > 0:
+                    right -= 1
+                else:
+                    answer.append([n, nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
         return answer
-    
