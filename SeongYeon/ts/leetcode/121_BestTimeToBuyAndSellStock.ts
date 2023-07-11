69a8f9958bf8dbf1db7b7e3231d062e6d296ccdd
CheeseB <2489ckckck@naver.com>
2023-07-11 22:12:35 +0900

    다른사람 풀이

--- SeongYeon/ts/leetcode/121_BestTimeToBuyAndSellStock.ts
+++ SeongYeon/ts/leetcode/121_BestTimeToBuyAndSellStock.ts
@@ -2,10 +2,10 @@ function maxProfit(prices: number[]): number {
   let maxProfit = 0;
   let minPrice = prices[0];
 
-  for (let i = 1; i < prices.length; i++) {
-    if (prices[i] < minPrice) minPrice = prices[i];
-    else maxProfit = Math.max(maxProfit, prices[i] - minPrice);
-  }
+  prices.forEach((price) => {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  });
 
   return maxProfit;
 }
