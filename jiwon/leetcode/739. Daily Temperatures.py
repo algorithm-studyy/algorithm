55ac9bdb44c856c704e9eaa53ba2f710d4ab77b4
ambosing <ambosing_@naver.com>
2023-07-26 22:09:19 +0900

    Create 739. Daily Temperatures.py

---
+++ jiwon/leetcode/739. Daily Temperatures.py
@@ -0,0 +1,15 @@
+# stack에 아무것도 없으면 삽입
+# while(stack[-1] < t) stack.pop() count += 1 [1...] 
+# stack.append(t)
+# while len(answer) < len(temps) stack.append(0)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer, stack = [0] * len(temperatures), [] 
+        for i, t in enumerate(temperatures):
+            count = 1
+            while stack and stack[-1][0] < t:
+                value, idx = stack.pop()
+                answer[idx] = i - idx
+            stack.append([t, i])
+
+        return answer
