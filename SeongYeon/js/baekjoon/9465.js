e244556a635becad17850d387c7f41d6248a77b1
CheeseB <2489ckckck@naver.com>
2023-08-02 17:34:47 +0900

    문제푸는중

---
+++ SeongYeon/js/baekjoon/9465.js
@@ -0,0 +1,11 @@
+// input
+const fs = require('fs');
+const inputFile = 'test/input.txt';
+const input = fs.readFileSync(inputFile).toString().split('\n');
+
+// solution
+let result = '';
+result = input;
+
+// output
+console.log(result);
