f7dc234469181b352ad7053f1d3d2ff760879c10
ZENO <zeno1030@naver.com>
2023-08-09 22:12:52 +0900

    counter를 사용하여 시간초과 해결

--- JiHwan/BJ/14225.py
+++ JiHwan/BJ/14225.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from collections import Counter
 
 n = int(input())
 s = list(map(int, input().split()))
@@ -9,6 +10,10 @@ for i in range(1, n+1):
     for j in comb_list:
         sum_list.append(sum(j))
 
-for i in range(1, max(sum_list)+1):
-    if i not in sum_list:
+
+list_count = Counter(sum_list)
+
+for i in range(1, 2**n + 1):
+    if i not in list_count:
         print(i)
+        break
