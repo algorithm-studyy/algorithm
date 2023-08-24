e7df0579cab0ac407620a21d32cad00396b930c7
ambosing <ambosing_@naver.com>
2023-08-24 22:39:22 +0900

    Create 16198.py

---
+++ jiwon/boj/python/16198.py
@@ -0,0 +1,17 @@
+# N개의 에너지 구슬
+# 1 2 3 4
+# x = 3 -> w = 2 * 4
+# 1 2 4
+# x = 2 -> w += 1 * 4
+from sys import stdin
+
+
+def dfs():
+
+
+
+def solution():
+    n = int(stdin.readline())
+    ws = list(map(int, stdin.readline().split()))
+    for i in range(1, len(ws) - 1):
+        dfs()
