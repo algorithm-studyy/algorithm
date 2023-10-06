ab0fb8c98cecd50bd274e38e5083c569256e1ace
ambosing <ambosing_@naver.com>
2023-10-06 21:35:31 +0900

    섬 연결하기 해결.. (프림)
    프림이랑 크루스칼 한 번 보기

--- jiwon/programmers/python/connecting_island.py
+++ jiwon/programmers/python/connecting_island.py
@@ -1,24 +1,26 @@
 # n개의 섬 costs
 # 다리를 여러 번 건너도 도달할 수만 있으면 통행 가능
-from heapq import heappush, heappop
+from heapq import heappush, heappop, heapify
+from collections import defaultdict
 
 
 def solution(n, costs):
-    g = {k:dict() for k in range(n)}
+    answer = 0
+    g = defaultdict(list)
     for v1, v2, cost in costs:
-        g[v1][v2] = cost
-        g[v2][v1] = cost
-    d = [float('inf')] * n
-    heap = []
-    heappush(heap, (0, 0))
-    d[0] = 0
+        g[v1].append([cost, v1, v2])
+        g[v2].append([cost, v2, v1])
+    visit = set([0])
+    heap = g[0]
+    heapify(heap)
     while heap:
-        distance, current  = heappop(heap)
-        if d[current] < distance:
+        distance, current, destination = heappop(heap)
+        if destination in visit:
             continue
-        for e in g[current]:
-            nd = g[current][e]
-            if nd < d[e]:
-                d[e] = nd
-                heappush(heap, (nd, e))
-    return sum(d)
+        visit.add(destination)
+        answer += distance
+        for edge in g[destination]:
+            if edge[2] not in visit:
+                heappush(heap, edge)
+
+    return answer
