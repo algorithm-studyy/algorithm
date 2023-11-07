63428e1f5de1112e0f3cdee857f65c337147704c
ambosing <ambosing_@naver.com>
2023-11-07 16:08:01 +0900

    Create 1697_2.py

---
+++ jiwon/boj/python/1697_2.py
@@ -0,0 +1,25 @@
+from collections import deque
+from sys import stdin
+
+
+def bfs(s):
+    q = deque([s])
+    v[s] = 0
+    while q:
+        x = q.popleft()
+        if x == k:
+            print(v[x])
+            return
+        for i in a:
+            nx = x * 2 if i == 2 else x + i
+            if not v.get(nx) and 0 <= nx <= max_val + 1:
+                v[nx] = v[x] + 1
+                q.append(nx)
+
+
+if __name__ == '__main__':
+    n, k = map(int, stdin.readline().split())
+    max_val = max(n, k)
+    a = [-1, 1, 2]
+    v = dict()
+    bfs(n)
