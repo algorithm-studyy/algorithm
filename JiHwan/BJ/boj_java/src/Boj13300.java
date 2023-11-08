73eab6742fccfc05fa81475845049b7e78cf1129
ZENO <zeno1030@naver.com>
2023-11-08 22:32:31 +0900

    1012 solved

---
+++ JiHwan/BJ/boj_java/src/Boj13300.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Boj13300 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int room = sc.nextInt();
+
+        for (int i = 0; i <num; i++) {
+
+
+        }
+    }
+}
