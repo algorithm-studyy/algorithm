2466ac18d0e1c58739c517e5e30a5cf6a1fcbca5
ZENO <zeno1030@naver.com>
2023-12-14 22:31:25 +0900

    solved

--- JiHwan/BJ/15663.py
+++ JiHwan/BJ/15663.py
@@ -1,11 +1,21 @@
-from itertools import permutations
+def back(start):
+    if len(answer) == m:
+        print(" ".join(map(str, answer)))
+        return
+    prev = 0
+    for i in range(n):
+        if not visited[i] and prev != nums[i]:
+            visited[i] = True
+            answer.append(nums[i])
+            prev = nums[i]
+            back(i + 1)
+            visited[i] = False
+            answer.pop()
 
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
     nums = sorted(list(map(int, input().split())))
-    answer = set()
-    for i in permutations(nums, m):
-        answer.add(" ".join(map(str, i)))
-    for a in sorted(answer):
-        print(a)
+    answer = []
+    visited = [False] * n
+    back(0)
