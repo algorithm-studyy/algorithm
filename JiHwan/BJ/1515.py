02dd953157499bfaf0fe1fd6514d9b6299a92df1
ZENO <zeno1030@naver.com>
2024-07-31 11:30:26 +0900

    solved

---
+++ JiHwan/BJ/1515.py
@@ -0,0 +1,12 @@
+n = input()
+answer = 0
+while True:
+    answer += 1
+    num = str(answer)
+    while len(num) > 0 and len(n) > 0:
+        if num[0] == n[0]:
+            n = n[1:]
+        num = num[1:]
+    if n == '':
+        print(answer)
+        break
