351d0bc39889b7b755648016c2be4de2ce91c08f
ZENO <zeno1030@naver.com>
2024-09-05 16:27:09 +0900

    solved

--- JiHwan/BJ/3758.py
+++ JiHwan/BJ/3758.py
@@ -1,14 +1,21 @@
 T = int(input())
-#팀의 수, 문제 수, 내 팀ID, 로그 엔트리개수
-
 for _ in range(T):
+
     n, k, t, m = map(int, input().split())
-    arr = [[0 for _ in range(n + 1)] for _ in range(k + 2)]
-    for _ in range(m):
-        i, j, s = map(int, input().split())
-        if arr[j][i] <= s:
-            arr[j][i] = s
-            arr[k + 1][i] += 1
-        else:
-            arr[k + 1][i] += 1
-        print(arr)
+
+    dic = {i: {"score": [0] * (k + 1), "submit": 0, "time": 0} for i in range(1, n + 1)}
+    print(dic)
+    idx = 0
+    for log_count in range(m):
+        i, j, s = list(map(int, input().split()))
+
+        dic[i]["score"][j] = max(dic[i]["score"][j], s)
+        dic[i]["submit"] += 1
+        dic[i]["time"] = idx
+        idx += 1
+
+    ord_dic = sorted(dic.items(), key=lambda item: (-sum(item[1]["score"]), item[1]["submit"], item[1]["time"]))
+
+    for i in range(len(ord_dic)):
+        if ord_dic[i][0] == t:
+            print(i + 1)
